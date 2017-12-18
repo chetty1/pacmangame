2c84eafe01a8e8ddf19c41a4eca1b5372ce2be7b
chetty1 <chetty365@gmail.com>
2017-12-18 09:58:04 +0200

    Update objloader.cpp

--- pacmangame/objloader.cpp
+++ pacmangame/objloader.cpp
@@ -1,9 +1,5 @@
 
-//This example program is created by thecplusplusuy for demonstration purposes. It's a simple 3D model loader (wavefront (.obj)), which is capable to load materials and UV textures:
-//http://www.youtube.com/user/thecplusplusguy
-//Free source, modify if you want, LGPL licence (I guess), I would be happy, if you would not delete the link
-//so other people can see the tutorial
-//this file is the objloader.cpp
+
 #include "objloader.h"
 #include "imageloader.h"
         //nothing to explain here
